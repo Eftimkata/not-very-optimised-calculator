78017802ad5289f4350b794df94f14f5d2dce661
Eftimkata <eftimij1novakovski@gmail.com>
2024-11-08 17:42:15 +0100

    ADDING THE PROJECT
    No need to say more look it in
    ITS IN PYTHON THO

---
+++ nvoti.py
@@ -0,0 +1,43 @@
+def cancel():
+    pass
+
+def again():
+    ask = input("Another equasion? ")
+    if ask == "no" or ask == "-":
+        cancel()
+    else:
+        calculator_module()
+
+def calculator_module():
+    v1 = float(input("Enter value 1: "))
+    v2 = float(input("Enter value 2: "))
+    
+    print("\n Possible options:\n\n  +   -   *   /   \n\n or \n\n   plus   minus   times   devide  \n")
+    what_to_do = input("What equasion: ")
+    what_to_do = what_to_do.lower()
+
+    
+    if what_to_do == "plus" or what_to_do == "+":
+        result = v1 + v2
+        print(result)
+
+        again()
+    elif what_to_do == "minus" or what_to_do  == "-":
+        v1 - v2
+        print(result)
+
+        again()
+    elif what_to_do == "times" or what_to_do == "*":
+        result = v1 * v2
+        print(result)
+
+        again()
+    elif what_to_do == "devide" or what_to_do == "/":
+        result = v1 / v2
+        print(result)
+
+        again()
+    else:
+        print("That is not valid choice try: \n  +  -  /  *  \n  plus  minus  times  devide  ")
+
+
---
+++ you tought it was over.py
@@ -0,0 +1,11 @@
+from nvoti import *
+def run_the_thing():
+    calculator_module()
+
+def run_runner():
+    run_the_thing()
+
+def run_runner_runner():
+    run_runner()
+
+run_runner_runner()
